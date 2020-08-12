de395bef6bf7960a04ad429cd65374b81baf0993
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-08-12 22:53:28 +0100

    Add files via upload

--- gestion_document_app/admin.py
+++ gestion_document_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Depot,Article,Departement,Devise,Fournisseur,Famille
+from .models import(Depot,Article,Departement,Devise,Fournisseur,Famille,Document)
 # Register your models here.
 
 admin.site.register(Depot)
@@ -8,6 +8,7 @@ admin.site.register(Devise)
 admin.site.register(Departement)
 admin.site.register(Fournisseur)
 admin.site.register(Famille)
+admin.site.register(Document)
 
 
 
--- gestion_document_app/forms.py
+++ gestion_document_app/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import *
 
 class DepotForm(forms.ModelForm):
@@ -38,3 +42,35 @@ class FournisseurForm(forms.ModelForm):
     class Meta:
         model=Fournisseur
         fields=["fournisseur_id","fournisseur_intitule","fournisseur_adresse","fournisseur_telephone","fournisseur_email"]
+
+
+class UserCreateForm(UserCreationForm):
+
+    class Meta:
+        fields = ('username','email','password1')
+        model = get_user_model()
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model=Document
+        fields=["document_id","document_piece","document_type","document_date","fournisseur_id","document_devise","document_reference"]
+        widgets = {
+            'document_date': DateInput()
+        }
+class DocumentFormValid1(forms.ModelForm):
+    
+    class Meta:
+        model=Document
+        fields=["document_id","numero_depot","document_date_livraison","document_cours","departement_id"]
+        widgets = {
+            'document_date_livraison': DateInput()
+        }
+        
+class DocumentFormValid2(forms.ModelForm):
+    class Meta:
+        model=Document
+        fields=["document_id","article_id","document_P_U_DEV","document_QT","document_taxe","document_remise"]
+        
